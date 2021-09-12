1bfa49c6b9c7ebfa92ad2a428d382425ad4e1370
alexandru <alexandru.palcuie1802@gmail.com>
2021-09-12 16:04:14 +0300

    First Writey user-service app commit

---
+++ src/main/java/alex/writey/userservice/WriteyUserServiceApplication.java
@@ -0,0 +1,13 @@
+package alex.writey.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WriteyUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WriteyUserServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/alex/writey/userservice/WriteyUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package alex.writey.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WriteyUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
